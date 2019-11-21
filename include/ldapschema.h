a9d4da27870ca1eed8f58230ebc2cf213484b2a0
David M. Syzdek <david@syzdek.net>
2019-11-20 19:21:10 -0900

    libldapschema: adjusting prototypes for sorting functions to prevent duplicate delcarations of inline fuctions

--- include/ldapschema.h
+++ include/ldapschema.h
@@ -445,6 +445,7 @@ ldapschema_print_syntaxes(
 // sort functions //
 //----------------//
 #pragma mark sort functions
+#ifndef _LIB_LIBLDAPSCHEMA_H
 
 _LDAPSCHEMA_F int
 ldapschema_extension_cmp(
@@ -461,6 +462,7 @@ ldapschema_syntax_cmp(
          const LDAPSchemaSyntax * s1,
          const LDAPSchemaSyntax * s2 );
 
+#endif
 
 LDAPSCHEMA_END_C_DECLS
 #endif /* end of header */
