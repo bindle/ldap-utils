3bbdde091f8696368a12e9174ab30b842c6d0fe3
David M. Syzdek <david@syzdek.net>
2019-11-21 15:34:38 -0900

    libldapschema: adjusting whitespace

--- lib/libldapschema/libldapschema.h
+++ lib/libldapschema/libldapschema.h
@@ -72,93 +72,93 @@
 /// LDAP schema descriptor state
 struct ldap_schema
 {
-   int32_t              errcode;          ///< last error code
-   int32_t              pad32;
-   LDAPSchemaPointer  * oids;             ///< array of all known oids
-   size_t               oids_len;         ///< length of oids array
-   LDAPSchemaSyntax  ** syntaxes;         ///< array of syntaxes
-   size_t               syntaxes_len;     ///< length of syntaxes array
-   LDAPSchemaAttributeType  ** attrs;     ///< array of attributeTypes
-   size_t               attrs_len;        ///< length of attributeTypes array
-   LDAPSchemaSpec    ** specs;            ///< sorted list of specifications
-   size_t               specs_len;        ///< length of sorted list of specifications
+   int32_t                                errcode;          ///< last error code
+   int32_t                                pad32;
+   LDAPSchemaPointer                    * oids;             ///< array of all known oids
+   size_t                                 oids_len;         ///< length of oids array
+   LDAPSchemaSyntax                    ** syntaxes;         ///< array of syntaxes
+   size_t                                 syntaxes_len;     ///< length of syntaxes array
+   LDAPSchemaAttributeType             ** attrs;            ///< array of attributeTypes
+   size_t                                 attrs_len;        ///< length of attributeTypes array
+   LDAPSchemaSpec                      ** specs;            ///< sorted list of specifications
+   size_t                                 specs_len;        ///< length of sorted list of specifications
 };
 
 
 /// LDAP schema extension
 struct ldap_schema_extension
 {
-   char             * extension;
-   char            ** values;
-   size_t             values_len;
+   char                                 * extension;
+   char                                ** values;
+   size_t                                 values_len;
 };
 
 
 /// LDAP schema base data model
 struct ldap_schema_model
 {
-   size_t                  size;        ///< size of data struct
-   uint32_t                type;        ///< LDAP schema data type
-   uint32_t                flags;
-   char                  * definition;  ///< defintion of object
-   char                  * oid;         ///< oid of object
-   char                  * desc;        ///< description of object;
-   const LDAPSchemaSpec  * spec;
-   LDAPSchemaExtension  ** extensions;
-   size_t                  extensions_len;
+   size_t                                 size;             ///< size of data struct
+   uint32_t                               type;             ///< LDAP schema data type
+   uint32_t                               flags;
+   char                                 * definition;       ///< defintion of object
+   char                                 * oid;              ///< oid of object
+   char                                 * desc;             ///< description of object;
+   const LDAPSchemaSpec                 * spec;
+   LDAPSchemaExtension                 ** extensions;
+   size_t                                 extensions_len;
 };
 
 
 /// LDAP schema syntax
 struct ldap_schema_syntax
 {
-   LDAPSchemaModel         model;
-   regex_t                 re;
+   LDAPSchemaModel                        model;
+   regex_t                                re;
 };
 
 
 /// LDAP schema objectclass
 struct ldap_schema_objectclass
 {
-   LDAPSchemaModel              model;
-   size_t                       names_len;
-   size_t                       must_len;
-   size_t                       may_len;
-   size_t                       all_must_len;
-   size_t                       all_may_len;
-   LDAPSchemaObjectclass      * sup;
-   char                      ** names;
-   LDAPSchemaAttributeType   ** must;
-   LDAPSchemaAttributeType   ** may;
-   LDAPSchemaAttributeType   ** all_must;
-   LDAPSchemaAttributeType   ** all_may;
+   LDAPSchemaModel                        model;
+   size_t                                 names_len;
+   size_t                                 must_len;
+   size_t                                 may_len;
+   size_t                                 all_must_len;
+   size_t                                 all_may_len;
+   LDAPSchemaObjectclass                * sup;
+   char                                ** names;
+   LDAPSchemaAttributeType             ** must;
+   LDAPSchemaAttributeType             ** may;
+   LDAPSchemaAttributeType             ** all_must;
+   LDAPSchemaAttributeType             ** all_may;
 };
 
 
 union ldap_schema_pointer
 {
-   LDAPSchemaModel          * model;
-   LDAPSchemaSyntax         * syntax;
-   LDAPSchemaObjectclass    * objectclass;
-   LDAPSchemaAttributeType  * attributetype;
-   LDAPSchemaMatchingRule   * matchingrule;
+   LDAPSchemaModel                      * model;
+   LDAPSchemaSyntax                     * syntax;
+   LDAPSchemaObjectclass                * objectclass;
+   LDAPSchemaAttributeType              * attributetype;
+   LDAPSchemaMatchingRule               * matchingrule;
 };
 
 
 /// LDAP schema attributeType
 struct ldap_schema_attributetype
 {
-   LDAPSchemaModel                   model;
-   LDAPSchemaSyntax                * syntax;
-   uint64_t                          usage;
-   size_t                            names_len;
-   size_t                            allowed_by_len;
-   size_t                            required_by_len;
-   uint64_t                          min_upper;
-   char                            * sup_name;
-   char                           ** names;
-   LDAPSchemaObjectclass          ** allowed_by;
-   LDAPSchemaObjectclass          ** required_by;
+   LDAPSchemaModel                        model;
+   LDAPSchemaSyntax                     * syntax;
+   uint64_t                               usage;
+   size_t                                 names_len;
+   size_t                                 allowed_by_len;
+   size_t                                 required_by_len;
+   uint64_t                               min_upper;
+   char                                 * sup_name;
+   char                                ** names;
+   LDAPSchemaObjectclass               ** allowed_by;
+   LDAPSchemaObjectclass               ** required_by;
 };
 
 
